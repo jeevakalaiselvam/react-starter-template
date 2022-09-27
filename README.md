9cf38fc77290304aab8112a5e1326c8e2515f5d0
jeevakalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2022-09-27 15:54:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-starter-template
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-owe2a3)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Redux Toolkit Example</h1>
+    </div>
+  );
+}
